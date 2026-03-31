9c741ff5510af59120fa2bdb4c902fd9d07eed7c
Austen <delgatto@rocketmail.com>
2026-03-31 11:03:59 -0400

    Initial commit.

---
+++ database/db.js
@@ -0,0 +1,45 @@
+import * as SQLite from 'expo-sqlite';
+
+export const db = SQLite.openDatabase('workout.db');
+
+export const initDB = () => {
+    db.transaction(tx => {
+        tx.executeSql(`
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                email TEXT NOT NULL,
+                password TEXT NOT NULL
+            );
+        `);
+
+        tx.executeSql(`
+            CREATE TABLE IF NOT EXISTS templates (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER,
+                name TEXT,
+                FOREIGN KEY (user_id) REFERENCES users(id)
+            );
+        `);
+
+        tx.executeSql(`
+            CREATE TABLE IF NOT EXISTS workouts (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                group TExT NOT NULL
+            );
+        `);
+
+        tx.executeSql(`
+            CREATE TABLE IF NOT EXISTS template_workouts (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                template_id INTEGER,
+                workout_id INTEGER,
+                sets INTEGER,
+                reps INTEGER,
+                FOREIGN KEY (template_id) REFERENCES templates(id),
+                FOREIGN KEY (workout_id) REFERENCES workouts(id)
+            )
+        `);
+    });
+    tx.executeSql('PRAGMA foreign_keys = ON;');
+}
