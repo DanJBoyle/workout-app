1bbfbb347d2368868ad4b9ffe0c1e03c1cda1909
DanJBoyle <daniel.boyle328@gmail.com>
2026-04-03 10:59:40 -0400

    - Added save template modals - Added add button to exercise page

--- app/completion.tsx
+++ app/completion.tsx
@@ -1,14 +1,21 @@
+import SaveTemplateModal from "@/components/modals/saveTemplateModal";
 import Button from "@/components/UI/Button";
 import Container from "@/components/UI/Container";
 import Typography from "@/components/UI/Typography";
 import { router } from "expo-router";
+import { useState } from "react";
 
 export default function CompletionScreen() {
+  const [showSaveTemplate, setShowSaveTemplate] = useState(true);
+
   return (
     <Container>
       <Typography variant="title">Completion Screen</Typography>
       <Typography>Congratulations on completing your workout!</Typography>
-      <Button title="Save Template" onPress={() => router.push("/dashboard")} />
+      <SaveTemplateModal
+        visible={showSaveTemplate}
+        onClose={() => setShowSaveTemplate(false)}
+      />
       <Button
         title="Back to Dashboard"
         onPress={() => router.push("/dashboard")}
