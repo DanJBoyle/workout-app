4a9c47ca98127a667631eda643d6468825e11dc6
Austen <delgatto@rocketmail.com>
2026-04-07 11:19:45 -0400

    Added SQL insert and read functions for templates, exercises, and template_exercises

--- database/db.js
+++ database/db.js
@@ -22,7 +22,7 @@ export const initDB = () => {
         `);
 
     tx.executeSql(`
-            CREATE TABLE IF NOT EXISTS workouts (
+            CREATE TABLE IF NOT EXISTS exercises (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 name TEXT NOT NULL,
                 group TEXT NOT NULL
@@ -30,27 +30,29 @@ export const initDB = () => {
         `);
 
     tx.executeSql(`
-            CREATE TABLE IF NOT EXISTS template_workouts (
+            CREATE TABLE IF NOT EXISTS template_exercises (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 template_id INTEGER,
-                workout_id INTEGER,
+                exercise_id INTEGER,
                 sets INTEGER,
                 reps INTEGER,
                 FOREIGN KEY (template_id) REFERENCES templates(id),
-                FOREIGN KEY (workout_id) REFERENCES workouts(id)
+                FOREIGN KEY (exercise_id) REFERENCES exercises(id)
             )
         `);
 
     tx.executeSql("PRAGMA foreign_keys = ON;");
   });
 };
-export const registerUser = (emailInput, passwordInput) => {
+
+//insert into users
+export const registerUser = async (emailInput, passwordInput) => {
     try  {
         const existingUser = await findByEmail(emailInput);
         if (existingUser) {
             throw new Error("Email already exists");
         }
-    }
+
     return new Promise((resolve, reject) => {
         db.transaction((tx) => {
             tx.executeSql(
@@ -69,8 +71,12 @@ export const registerUser = (emailInput, passwordInput) => {
             );
         });
     })
+    } catch (error) {
+        throw error;
+    }
 };
 
+//get one user
 export const findByEmail = (emailInput) => {
 return new Promise((resolve, reject) => {
     db.transaction((tx) =>{
@@ -91,3 +97,102 @@ return new Promise((resolve, reject) => {
         );
     });
 })};
+
+//insert into templates
+export const createTemplate = (name, user_id) => {
+    return new Promise((resolve, reject) => {
+        db.transaction((tx) => {
+            tx.executeSql(
+            `INSERT INTO templates (name, user_id) VALUES (?, ?)`,
+            [name, user_id],
+            (_, result) => {
+                if (result.rowsAffected > 0) {
+                    resolve(result.insertId);
+                } else {
+                    reject(new Error("Failed to create new template"));
+                }
+            },
+                (_, error) => {
+                    reject(error);
+                }
+            );
+        });
+    })
+};
+
+//insert into exercises
+export const createExercise = (name, group) => {
+  return new Promise((resolve, reject) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        `INSERT INTO exercises (name, group) VALUES (?, ?)`,
+        [name, group],
+        (_, result) => {
+          if (result.rowsAffected > 0) {
+            resolve(result.insertId);
+          } else {
+            reject(new Error("Failed to create exercise"));
+          }
+        },
+        (_, error) => reject(error)
+      );
+    });
+  });
+};
+
+//get all exercises
+export const getAllExercises = () => {
+  return new Promise((resolve, reject) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        `SELECT * FROM exercises`,
+        [],
+        (_, result) => {
+          resolve(result.rows._array);
+        },
+        (_, error) => reject(error)
+      );
+    });
+  });
+};
+
+//get exercises from one group
+export const getExercisesByGroup = (group) => {
+  return new Promise((resolve, reject) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        `SELECT * FROM exercises WHERE group = ?`,
+        [group],
+        (_, result) => resolve(result.rows._array),
+        (_, error) => reject(error)
+      );
+    });
+  });
+};
+
+//insert into template_exercises
+export const addExerciseToTemplate = (
+  template_id,
+  exercise_id,
+  sets,
+  reps
+) => {
+  return new Promise((resolve, reject) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        `INSERT INTO template_exercises
+         (template_id, exercise_id, sets, reps)
+         VALUES (?, ?, ?, ?)`,
+        [template_id, exercise_id, sets, reps],
+        (_, result) => {
+          if (result.rowsAffected > 0) {
+            resolve(result.insertId);
+          } else {
+            reject(new Error("Failed to add exercise to template"));
+          }
+        },
+        (_, error) => reject(error)
+      );
+    });
+  });
+};
